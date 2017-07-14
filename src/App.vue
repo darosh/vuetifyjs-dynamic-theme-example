05f78c302f20801eae4f323614dfad262b5ecefc
Jan Forst <ensonador@gmail.com>
2017-07-14 15:40:23 +0200

    fix links

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
           <v-flex xs12 sm12 md12>
             <v-card>
               <blockquote>
-                This is test of dynamic CSS variables usage in Vuetify. Source code available on <a href="">GitHub</a>.
+                This is test of dynamic CSS variables usage in <a href="https://vuetifyjs.com">Vuetify</a>. Source code available on <a href="https://github.com/darosh/vuetifyjs-dynamic-theme-example">GitHub</a>.
               </blockquote>
             </v-card>
           </v-flex>
